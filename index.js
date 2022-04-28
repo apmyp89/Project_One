da798bdbb82167f9a5687585083022b0c8663bce
Artur <apmyp89@ya.ru>
2022-04-28 18:48:07 +0300

    Lesson_1

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-"use strick";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
 
-let a = 13;
-console.log(a);
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+
+    },
+    actors: {
+
+    },
+    genres: [],
+    privat: false,
+};
+
+const a = prompt('Какой последний фильм вы посмотрели?'),
+      b = prompt('Ваша оценка фильму?'),
+      c = prompt('Какой последний фильм вы посмотрели?'),
+      d = prompt('Ваша оценка фильму?');
+
+
+
+personalMovieDb.movies[a] = b;
+personalMovieDb.movies[c] = d;
+
+
+console.log(personalMovieDB);
